1f2b8f64014b8339645ca784e39a679270e344be
zl <wlbz2014@163.com>
2019-11-14 01:04:12 +0800

    函数饮用

--- src/main/kotlin/grammar.kt
+++ src/main/kotlin/grammar.kt
@@ -1,6 +1,12 @@
 fun main() {
     println("hello world")
     println(sum(1, 2 + 4))
+    val ssum = ::sum
+    println(ssum(1,2))
+    println(ssum?.invoke(2, 3))
+    val summ:(Int,Int) ->Int = {a,b -> a+b}
+    println(summ(3, 4))
+    println(summ?.invoke(4, 5))
 }
 
 fun sum(a: Int, b: Int) = a + b
